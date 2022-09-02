b360cf541501aef926a9181d5db5da50eb208179
42ghan <ghan@student.42seoul.kr>
2022-09-02 15:46:07 +0900

    🔨 chore : build & test 환경 설정
    Co-authored-by: Jisu Kim <jiskim@student.42seoul.kr>
    Co-authored-by: Yongjun Lee <yongjule@student.42seoul.kr>

---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(Example, Test) { EXPECT_EQ(0, 1); }
+TEST(Example, Test1) { EXPECT_EQ(0, 1); }
+TEST(Example, Test2) { EXPECT_EQ(0, 1); }
+TEST(Example1, Test) { EXPECT_EQ(0, 1); }
+TEST(Example2, Test) { EXPECT_EQ(0, 1); }
